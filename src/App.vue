5825b549b5a0d17feb45a8d5eabdd8fdcb4b8c65
PHenryy <qwerty.code24@gmail.com>
2019-07-30 13:22:50 +0800

    Add Pagination

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,20 @@
 <template>
-  <div id="app">
-    <Row>
-      <Column :span="3">
-        <div class="square square1"></div>
-      </Column>
-      <Column>
-        <div class="square square2"></div>
-      </Column>
-      <Column>
-        <div class="square square3"></div>
-      </Column>
-    </Row>
-
-    <br />
-
-    <s-view>
-      <s-left>
-        <span class="title">title</span>
-      </s-left>
-      <s-center>
-        <span class="content">content</span>
-      </s-center>
-      <s-right class="s_right">
-        <span class="arrow">arrow</span>
-      </s-right>
-    </s-view>
+  <div id="app" class="p-20">
+    <Pagination :total-length="100" v-model="current" />
   </div>
 </template>
 
 <script>
-import Row from "@/components/Row.vue";
-import Column from "@/components/Column.vue";
-import View from "@/components/View.vue";
-import Left from "@/components/Left.vue";
-import Center from "@/components/Center.vue";
-import Right from "@/components/Right.vue";
+import Pagination from "@/components/Pagination/index";
 export default {
   name: "app",
   components: {
-    Row,
-    Column,
-    "s-view": View,
-    "s-left": Left,
-    "s-center": Center,
-    "s-right": Right
+    Pagination
+  },
+  data() {
+    return {
+      current: 7
+    };
   }
 };
 </script>
-<style lang="scss">
-@import "@/assets/scss/grid.scss";
-
-.square {
-  height: 50px;
-}
-.square1 {
-  background-color: #ffd98e;
-}
-.square2 {
-  background-color: #ffb677;
-}
-.square3 {
-  background-color: #ff8364;
-}
-.title {
-  display: inline-block;
-  height: 50px;
-  background-color: #ffd98e;
-}
-.content {
-  display: inline-block;
-  background-color: #ffb677;
-}
-.arrow {
-  display: inline-block;
-  background-color: #ff8364;
-}
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        cobalt: "#3a58ff",
+        "dark-1": "#2c3242",
+        "dark-2": "#747881"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
